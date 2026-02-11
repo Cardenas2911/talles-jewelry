23a7f9036634dc89d5f072d9162989aa75f388f4
Nicolas Cardenas <nicolas.cardenas1129@gmail.com>
2026-02-10 21:42:13 -0500

    Updates

---
+++ _astro/paths.BCg1NTiL.js
@@ -0,0 +1,1 @@
+const r=e=>{const s="/talles-jewelry".replace(/\/$/,""),t=e.startsWith("/")?e:`/${e}`;return`${s}${t}`};export{r};
--- _astro/paths.DUiYo9p1.js
+++
@@ -1,1 +0,0 @@
-const n=e=>{const t=e.startsWith("/")?e.slice(1):e,s="/talles-jewelry";return`${s.endsWith("/")?s:`${s}/`}${t}`};export{n as r};
